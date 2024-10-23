3e1919e3679ac268af5e4d77709e05c1eba8376f
bertrand.gallice <bertrandgllc@gmail.com>
2024-10-23 10:55:22 +0200

    update

--- shinyproxy/deploy_secrets.sh
+++ shinyproxy/deploy_secrets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check for correct number of arguments
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 POSTGRES_USERNAME POSTGRES_PASSWORD POSTGRES_HOST POSTGRES_PORT POSTGRES_DB"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 POSTGRES_USERNAME POSTGRES_PASSWORD POSTGRES_HOST POSTGRES_PORT POSTGRES_DB POSTGRES_DB_V2"
     exit 1
 fi
 
@@ -11,10 +11,12 @@ POSTGRES_PASSWORD=$(echo -n "${2}" | base64)
 POSTGRES_HOST=$(echo -n "${3}" | base64)
 POSTGRES_PORT=$(echo -n "${4}" | base64)
 POSTGRES_DB=$(echo -n "${5}" | base64)
+POSTGRES_DB_V2=$(echo -n "${6}" | base64)
 
 # Substitute variables in k8s.yaml and apply
 kubectl apply -f <( sed -e "s|\${POSTGRES_USERNAME}|${POSTGRES_USERNAME}|g" \
     -e "s|\${POSTGRES_PASSWORD}|${POSTGRES_PASSWORD}|g" \
     -e "s|\${POSTGRES_HOST}|${POSTGRES_HOST}|g" \
     -e "s|\${POSTGRES_PORT}|${POSTGRES_PORT}|g" \
-    -e "s|\${POSTGRES_DB}|${POSTGRES_DB}|g" k8s-secrets.yaml )
+    -e "s|\${POSTGRES_DB}|${POSTGRES_DB}|g" \
+    -e "s|\${POSTGRES_DB_V2}|${POSTGRES_DB_V2}|g"  k8s-secrets.yaml )
